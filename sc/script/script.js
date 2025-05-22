f1dd393100b7bfd906ff08165dbf18fc02c68ab0
codewithtec.com <158636974+CodeWithTec@users.noreply.github.com>
2025-05-22 00:17:30 +0000

    First upload

---
+++ sc/script/script.js
@@ -0,0 +1,10 @@
+
+
+if(document.getElementById("load-section").style.display = "block"){
+    setTimeout(() => {
+        document.getElementById("load-section").style.display = "none";
+        document.getElementById("get-started").style.display = "block";
+        document.getElementById("get-started-1").style.display = "block";
+
+    }, 5000);
+};
